8540413b976169a26c852a273a85076c8c955565
Joel Ross <joelross@uw.edu>
2023-02-09 10:30:13 -0800

    End of react component lecture demo

--- react-components/src/index.js
+++ react-components/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import './index.css';
+import App from './components/App.js';
 
-//create an element using JSX
-const reactElem = <h1>Hello React!</h1>
+import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(reactElem);
+root.render(<App />);
