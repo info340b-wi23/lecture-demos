e8cf739635aa422d5d1397ddd15a8ea9393f3cee
Joel Ross <joelross@uw.edu>
2023-02-14 10:22:56 -0800

    End of react app demo

--- react-chat-1/src/components/App.js
+++ react-chat-1/src/components/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
 
+import { HeaderBar } from './HeaderBar.js';
+import { ChannelList } from './ChannelNav.js';
+import { ChatPane } from './ChatPane.js';
+
 function App(props) {
   return (
-    <div>Hello App</div>
+    <div className="container-fluid">
+      <HeaderBar />
+      <div className="row">
+        <div className="col-3">
+          <ChannelList />
+        </div>
+        <div className="col">
+          <ChatPane />
+        </div>
+      </div>
+    </div>
   );
 }
 
